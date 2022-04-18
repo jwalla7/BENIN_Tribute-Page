07a0758b6a280661ea7c79aeaf402c0efef97182
jwalla7 <j7wallace7@outlook.com>
2022-04-18 08:53:46 -0500

    apr 18 0853

--- JS/benin-main-window.js
+++ JS/benin-main-window.js
@@ -6,3 +6,13 @@ if (history.scrollRestoration) {
         window.scrollTo(0, 0);
     }
 }
+
+// PAGE RULER
+window.onresize = ruler;
+window.onload = ruler;
+
+function ruler() {
+    let myWidth = window.innerWidth;
+    let myHeight = window.innerHeight;
+    document.getElementById('table-of-contents-input').innerHTML = `Page Width: ${myWidth}, Height: ${myHeight}`;
+}
